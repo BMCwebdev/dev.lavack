03dad197895233ceb86fc5a4ced086232c6901ef
Brian <Brian@bmcwebdev.com>
2013-08-19 01:58:49 -0400

    replace PHP to log in

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'lavackblog');
+define('DB_NAME', 'aligator_lavack');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'aligator_lavack');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'lavack2013');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
