972c5e4be3d0f03c7ec860e57cde0e96e44ca85d
Brian <Brian@bmcwebdev.com>
2013-08-23 16:23:14 -0400

    ..

--- js/forms.js
+++ js/forms.js
@@ -11,7 +11,7 @@
 					successCl:'success',
 					successShow:'4000',
 					mailHandlerURL:'bat/MailHandler.php',
-					ownerEmail:'babyaligators@comcast.net',
+					ownerEmail:'brian@bmcwebdev.com',
 					stripHTML:true,
 					smtpMailServer:'localhost',
 					targets:'input,textarea',
@@ -182,6 +182,6 @@
 })(jQuery)
 $(window).load(function(){
 	$('#contact-form').forms({
-		ownerEmail:'babyaligators@comcast.net'
+		ownerEmail:'brian@bmcwebdev.com'
 	})
 })
