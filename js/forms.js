db04771e85adb0b1ef3676f5d6b4a563654968a5
Brian <Brian@bmcwebdev.com>
2013-08-23 16:26:28 -0400

    ..

--- js/forms.js
+++ js/forms.js
@@ -10,7 +10,7 @@
 					notRequiredCl:'notRequired',
 					successCl:'success',
 					successShow:'4000',
-					mailHandlerURL:'bat/MailHandler.php',
+					mailHandlerURL:'mail/MailHandler.php',
 					ownerEmail:'brian@bmcwebdev.com',
 					stripHTML:true,
 					smtpMailServer:'localhost',
