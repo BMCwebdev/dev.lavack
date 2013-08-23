cb130cde242a53a8aa352de545503548f15e0ff3
Brian <Brian@bmcwebdev.com>
2013-08-23 15:38:24 -0400

    email form

--- js/forms.js
+++ js/forms.js
@@ -11,7 +11,7 @@
 					successCl:'success',
 					successShow:'4000',
 					mailHandlerURL:'bat/MailHandler.php',
-					ownerEmail:'support@guardlex.com',
+					ownerEmail:'babyaligators@comcast.net',
 					stripHTML:true,
 					smtpMailServer:'localhost',
 					targets:'input,textarea',
